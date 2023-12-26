3dd6bd0dc5ecf104f273660895d67a5b129161ee
Fernando Negrete Pimentel <fnegrete9510@gmail.com>
2023-12-26 12:14:34 -0600

    Initial Commit

---
+++ FirstAppSwifUI/FirstAppSwifUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppSwifUIApp.swift
+//  FirstAppSwifUI
+//
+//  Created by Fernando Negrete Pimentel on 26/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppSwifUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
