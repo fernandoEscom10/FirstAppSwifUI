a0a51464f51acee12dace874837ca273c3d156ea
Fernando Negrete Pimentel <fnegrete9510@gmail.com>
2023-12-27 17:31:55 -0600

    3.5 missing the second part of this chapter

---
+++ FirstAppSwifUI/Categories/CategoryHome.swift
@@ -0,0 +1,36 @@
+//
+//  CategoryHome.swift
+//  FirstAppSwifUI
+//
+//  Created by Fernando Negrete Pimentel on 27/12/23.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    @Environment(ModelData.self) var modelData
+    var body: some View {
+        NavigationSplitView {
+            List {
+                modelData.features[0].image
+                     .resizable()
+                     .scaledToFill()
+                     .frame(height: 200)
+                     .clipped()
+                     .listRowInsets(EdgeInsets())
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
+                }
+                .listRowInsets(EdgeInsets())
+            }
+            .navigationTitle("Featured")
+         } detail: {
+             Text("Select a Landmark")
+         }
+    }
+}
+
+#Preview {
+    CategoryHome()
+        .environment(ModelData())
+}
